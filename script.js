1c2cd7f2eeb38cc25d14745f2205a96b4153716b
Jayu <jayeshhande00@gmail.com>
2022-04-19 17:38:43 +0530

    edit script

---
+++ script.js
@@ -0,0 +1,19 @@
+let username = document.querySelector("username");
+
+let email = document.querySelector("password");
+
+let password = document.querySelector("password");
+
+let form = document.querySelector("form");
+
+let errorMsg = document.querySelector(".error");
+
+let successIcon = document.querySelector(".success-icon");
+
+let failureIcon = document.querySelector("failure-icon");
+
+//my name is jayesh hande and a i live in  recently inn pune
+// in sinhagad college of sciece ;
+
+//Jacascriot code is used in eery javascript fraameqperks
+//also to finsd the error message in the form to creat a function to avoid error and in the purpose of arror less devices
